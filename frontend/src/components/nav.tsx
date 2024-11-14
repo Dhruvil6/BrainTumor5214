ea4de113d1c3671292bef699c9e86f080b8c1d25
Dhruvil6 <70736317+Dhruvil6@users.noreply.github.com>
2024-11-13 21:11:28 -0600

    Delete frontend directory

--- frontend/src/components/nav.tsx
+++
@@ -1,9 +0,0 @@
-export default function Nav() {
-    return (
-        <div className="w-full bg-[#036fc4]">
-            <div className="flex-center">
-                <h1 className="text-[100px] text-white">Brain Tumor Scan</h1>
-            </div>
-        </div>
-    );
-}
